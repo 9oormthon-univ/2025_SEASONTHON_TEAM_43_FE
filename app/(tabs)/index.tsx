b5a56dd9dbf533a80545f66875248dfa98722cb2
rngrhn4114 <is9206855@gmail.com>
2025-08-30 02:54:59 +0900

    init: nativewind 환경 세팅

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,9 +3,10 @@ import { Text, View } from "react-native";
 
 export default function TabOneScreen() {
   return (
-    <View>
-      <Text>Tab One</Text>
+    <View className="flex-1 items-center justify-center">
+      <Text className="text-2xl font-bold">Tab One</Text>
       <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
   );
 }
+
--- components/EditScreenInfo.tsx
+++ components/EditScreenInfo.tsx
@@ -1,19 +1,15 @@
 import React from "react";
-import { StyleSheet } from "react-native";
-
 import { Text, View } from "react-native";
 
 export default function EditScreenInfo({ path }: { path: string }) {
   return (
-    <View>
-      <View>
-        <Text>Open up the code for this screen:</Text>
-
+    <View className="flex-1">
+      <View className="mt-3 items-center">
+        <Text className="text-lg font-medium">Open up the code for this screen:</Text>
         <View>
-          <Text>{path}</Text>
+          <Text className="text-sm">{path}</Text>
         </View>
-
-        <Text>Change any of the text, save the file, and your app will automatically update.</Text>
+        <Text className="text-sm">Change any of the text, save the file, and your app will automatically update.</Text>
       </View>
     </View>
   );
