bd199c30154324e79032baa2a08969a88a8836dc
rngrhn4114 <is9206855@gmail.com>
2025-08-31 15:27:49 +0900

    style: 타이포 및 색 클래스 추가

--- app/login.tsx
+++ app/login.tsx
@@ -2,19 +2,17 @@ import { router } from "expo-router";
 import { Text, View, TouchableOpacity } from "react-native";
 
 export default function LoginScreen() {
-  // 로그인 버튼 클릭 시 로직 작성
   const handleLogin = () => {
-    // 카카오 로그인 로직
-    console.log("카카오 로그인");
+    // TODO: 카카오 로그인 로직 추가
     router.push("/agreement");
   };
 
   return (
     <View className="flex-1 items-center justify-center p-4">
-      <Text className="text-2xl font-bold mb-4 font-suit-bold">로고</Text>
-      <Text className="text-2xl font-bold mb-4 font-suit-bold">로그인 화면</Text>
-      <TouchableOpacity className="mt-4 p-2 bg-blue-500 rounded" onPress={handleLogin}>
-        <Text className="text-white font-suit-bold">카카오로 로그인하기</Text>
+      <Text className="text-2xl mb-4 display1">로고</Text>
+      <Text className="text-2xl mb-4 display1 text-point-4">로그인 화면</Text>
+      <TouchableOpacity className="p-3 bg-point-4 rounded" onPress={handleLogin}>
+        <Text className="text-white body1">카카오로 로그인하기</Text>
       </TouchableOpacity>
     </View>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,13 +4,107 @@ module.exports = {
   presets: [require("nativewind/preset")],
   theme: {
     extend: {
+      colors: {
+        black: "#282625",
+        white: "#FFFFFF",
+        point: {
+          1: "#FFF6D9",
+          2: "#F0CFB3",
+          3: "#D56E14",
+          4: "#A55615"
+        }
+      },
       fontFamily: {
         "suit-regular": ["SUITRegular", "sans-serif"],
         "suit-semibold": ["SUITSemiBold", "sans-serif"],
         "suit-bold": ["SUITBold", "sans-serif"]
+      },
+      fontSize: {
+        display1: "28px",
+        title1: "24px",
+        title2: "20px",
+        subtitle1: "18px",
+        subtitle2: "18px",
+        body1: "16px",
+        body2: "16px",
+        body3: "14px",
+        body4: "14px",
+        caption1: "12px"
+      },
+      lineHeight: {
+        140: "140%"
+      },
+      letterSpacing: {
+        0: "0px"
       }
     }
   },
-  plugins: []
+  plugins: [
+    function ({ addUtilities }) {
+      const newUtilities = {
+        ".display1": {
+          fontSize: "28px",
+          lineHeight: "140%",
+          letterSpacing: "0px",
+          fontFamily: "SUITBold, sans-serif"
+        },
+        ".title1": {
+          fontSize: "24px",
+          lineHeight: "140%",
+          letterSpacing: "0px",
+          fontFamily: "SUITBold, sans-serif"
+        },
+        ".title2": {
+          fontSize: "20px",
+          lineHeight: "140%",
+          letterSpacing: "0px",
+          fontFamily: "SUITBold, sans-serif"
+        },
+        ".subtitle1": {
+          fontSize: "18px",
+          lineHeight: "140%",
+          letterSpacing: "0px",
+          fontFamily: "SUITBold, sans-serif"
+        },
+        ".subtitle2": {
+          fontSize: "18px",
+          lineHeight: "140%",
+          letterSpacing: "0px",
+          fontFamily: "SUITRegular, sans-serif"
+        },
+        ".body1": {
+          fontSize: "16px",
+          lineHeight: "140%",
+          letterSpacing: "0px",
+          fontFamily: "SUITBold, sans-serif"
+        },
+        ".body2": {
+          fontSize: "16px",
+          lineHeight: "140%",
+          letterSpacing: "0px",
+          fontFamily: "SUITRegular, sans-serif"
+        },
+        ".body3": {
+          fontSize: "14px",
+          lineHeight: "140%",
+          letterSpacing: "0px",
+          fontFamily: "SUITBold, sans-serif"
+        },
+        ".body4": {
+          fontSize: "14px",
+          lineHeight: "140%",
+          letterSpacing: "0px",
+          fontFamily: "SUITRegular, sans-serif"
+        },
+        ".caption1": {
+          fontSize: "12px",
+          lineHeight: "140%",
+          letterSpacing: "0px",
+          fontFamily: "SUITSemibold, sans-serif"
+        }
+      };
+      addUtilities(newUtilities);
+    }
+  ]
 };
 
