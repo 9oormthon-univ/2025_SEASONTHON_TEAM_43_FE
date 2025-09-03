d890e8c2a4db509ecf81ee19099c0e24ad12ec19
rngrhn4114 <is9206855@gmail.com>
2025-09-04 02:40:03 +0900

    style: 하단 탭 바의 스타일을 조정하였습니다.

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -10,11 +10,24 @@ import DiaryInactive from "../../assets/images/ic_diary_inactive.svg";
 
 export default function TabLayout() {
   return (
-    <Tabs screenOptions={{ headerShown: false }}>
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        // 비활성/활성 색상 지정
+        tabBarActiveTintColor: "#D56E14", // point.3
+        tabBarInactiveTintColor: "#999999", // 비활성 회색 (원하는 값으로 변경)
+
+        // 라벨 공통 스타일
+        tabBarLabelStyle: {
+          fontFamily: "SUITSemiBold", // tailwind fontFamily에서 등록한 폰트 key
+          fontSize: 12 // caption1 사이즈처럼 직접 지정 가능
+        }
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: "Home",
+          title: "빵지도",
           tabBarIcon: ({ focused }) =>
             focused ? <MapActive width={28} height={28} /> : <MapInactive width={28} height={28} />
         }}
@@ -22,7 +35,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="explore"
         options={{
-          title: "Explore",
+          title: "빵코스",
           tabBarIcon: ({ focused }) =>
             focused ? <CourseActive width={28} height={28} /> : <CourseInactive width={28} height={28} />
         }}
@@ -30,7 +43,7 @@ export default function TabLayout() {
       <Tabs.Screen
         name="profile"
         options={{
-          title: "Profile",
+          title: "빵일기",
           tabBarIcon: ({ focused }) =>
             focused ? <DiaryActive width={28} height={28} /> : <DiaryInactive width={28} height={28} />
         }}
