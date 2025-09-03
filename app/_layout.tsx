f326ba26cd0705c5eb1555a30d8c03d883a9d886
rngrhn4114 <is9206855@gmail.com>
2025-09-03 22:34:33 +0900

    style: 타이포 및 색 클래스 추가하였습니다.

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,4 @@
 import "../global.css";
-import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { useFonts } from "expo-font";
 import { Stack } from "expo-router";
 import * as SplashScreen from "expo-splash-screen";
@@ -16,8 +15,9 @@ SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
-    ...FontAwesome.font
+    SUITBold: require("../assets/fonts/SUIT-Bold.ttf"),
+    SUITRegular: require("../assets/fonts/SUIT-Regular.ttf"),
+    SUITSemibold: require("../assets/fonts/SUIT-SemiBold.ttf")
   });
 
   useEffect(() => {
@@ -41,8 +41,6 @@ function RootLayoutNav() {
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="modal" options={{ presentation: "modal" }} />
     </Stack>
   );
 }
-
