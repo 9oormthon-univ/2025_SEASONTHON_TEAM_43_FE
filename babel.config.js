b1dca643ac1fc2cc0f5e625c714acd5d72fbc1c3
Hu <yunhu89@naver.com>
2025-09-05 07:32:35 +0900

    chore: 프로젝트 설정 및 타입 정의 업데이트하였습니다.

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,10 @@
-module.exports = function (api) {
+module.exports = function(api) {
   api.cache(true);
   return {
-    presets: [["babel-preset-expo", { jsxImportSource: "nativewind" }], "nativewind/babel"]
+    // 1. presets에는 Expo 기본 설정만 남겨둡니다.
+    presets: ['babel-preset-expo','nativewind/babel'],
+    
+    // 2. plugins 배열 안에 모든 플러그인을 올바른 순서로 넣어줍니다.
+    plugins: ['react-native-reanimated/plugin'],
   };
 };
